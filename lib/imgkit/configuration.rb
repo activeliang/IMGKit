7454bd6d5ae0a4d5966baf50e3641321d7cc4d68
activeliang <activeliang@gmail.com>
2020-02-12 21:23:00 +0800

    add logger info for method split

--- lib/imgkit/configuration.rb
+++ lib/imgkit/configuration.rb
@@ -12,6 +12,7 @@ class IMGKit
     def wkhtmltoimage
       @wkhtmltoimage ||= begin
         path = (using_bundler? ? `bundle exec which wkhtmltoimage` : `which wkhtmltoimage`)
+        logger.info "\033[41;30m#{path}\033[0m"
         path = (path.split("\n").compact.last || "").chomp
         path = '/usr/local/bin/wkhtmltoimage' if path.strip.empty?  # Fallback
         path
